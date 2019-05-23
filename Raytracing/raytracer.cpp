3b4c8b7becab425c76b3173f66194fe66058817e
vlc <clva1011@hs-karlsruhe.de>
2019-05-23 17:55:30 +0200

    first (rough) implementation of multithreaded raytracing

--- Raytracing/raytracer.cpp
+++ Raytracing/raytracer.cpp
@@ -7,6 +7,9 @@
 #include <cassert>
 #include <algorithm>
 #include <chrono>
+#include <thread>
+#include <utility>
+#include <mutex>
 
 #include "settings.h"
 
@@ -198,18 +201,65 @@ Vec3f trace(
     return surfaceColor + sphere->emissionColor;
 }
 
+Vec3f *image;
+std::vector<Sphere> spheres;
+std::vector<std::pair<unsigned, unsigned>> blocks;
+std::mutex mtx;
+
+void renderBlock(unsigned left, unsigned top){
+    float fov = 30.0, invWidth = 1 / float(width), invHeight = 1 / float(height);
+    float aspectratio = width / float(height);
+    float angle = tan(M_PI * 0.5 * fov / 180.);
+
+    for (unsigned y = top; y < BLOCK_HEIGHT + top; ++y) {
+        for (unsigned x = left; x < BLOCK_WIDTH + left; ++x) {
+            //std::cout << "(" << x << ", " << y << ")" << std::endl;
+            float xx = (2 * ((x + 0.5) * invWidth) - 1) * angle * aspectratio;
+            float yy = (1 - 2 * ((y + 0.5) * invHeight)) * angle;
+            Vec3f raydir(xx, yy, -1);
+            raydir.normalize();
+            image[y * width + x] = trace(Vec3f(0), raydir, spheres, 0);
+        }
+    }
+}
+
+void threadMain(){
+    mtx.lock();
+    while(blocks.size() > 0){
+        //std::cout << blocks.size() << " blocks remaining" << std::endl;
+        unsigned left = blocks.back().first;
+        unsigned top = blocks.back().second;
+        blocks.pop_back();
+        mtx.unlock();
+        renderBlock(left, top);
+        mtx.lock();
+    }
+    mtx.unlock();
+}
+
 // Render Funktion. Es wird ein Strahl pro Pixel berechnet und geben dessen Farbe zurück. 
 // Trifft der Strahl eine Kugel geben wird die Farbe der Kugel am Schnittpunkt zurückgegeben,
 // falls nicht wird die Hintergrundfarbe zurückgegeben.
-void render(const std::vector<Sphere> &spheres)
-{
-    unsigned width = 640, height = 480;
-    Vec3f *image = new Vec3f[width * height], *pixel = image;
-    float invWidth = 1 / float(width), invHeight = 1 / float(height);
-    float fov = 30, aspectratio = width / float(height);
-    float angle = tan(M_PI * 0.5 * fov / 180.);
+void render(){
+    //unsigned width = 640, height = 480;
+    image = new Vec3f[width * height];
+    /*float invWidth = 1 / float(width), invHeight = 1 / float(height);
+    float aspectratio = width / float(height);
+    float angle = tan(M_PI * 0.5 * fov / 180.);*/
     // Strahlen verfolgen
-    for (unsigned y = 0; y < height; ++y) {
+    if((width % BLOCK_WIDTH != 0) || (height % BLOCK_HEIGHT != 0)){
+        std::cout << "invalid blocksize, check settings.h" << std::endl;
+        return;
+    }
+
+    for(unsigned i = 0; i < width; i += BLOCK_WIDTH){
+        for(unsigned j = 0; j < height; j += BLOCK_HEIGHT){
+            //std::cout << "(" << i << ", " << j << ")" << std::endl;
+            blocks.push_back(std::make_pair(i, j));   //top left of each block
+        }
+    }
+
+    /*for (unsigned y = 0; y < height; ++y) {
         for (unsigned x = 0; x < width; ++x, ++pixel) {
             float xx = (2 * ((x + 0.5) * invWidth) - 1) * angle * aspectratio;
             float yy = (1 - 2 * ((y + 0.5) * invHeight)) * angle;
@@ -217,8 +267,17 @@ void render(const std::vector<Sphere> &spheres)
             raydir.normalize();
             *pixel = trace(Vec3f(0), raydir, spheres, 0);
         }
+    }*/
+
+    std::thread threads[THREAD_COUNT];
+    for(int i = 0; i < THREAD_COUNT; i++){
+        threads[i] = std::thread(threadMain);
     }
-    // Speichere das Ergebnis in die Datei 'raytraced' (flags unter windows behalten!)
+    for(int i = 0; i < THREAD_COUNT; i++){
+        threads[i].join();
+    }
+
+    // Speichere das Ergebnis in die Datei 'result' (flags unter windows behalten!)
     std::ofstream ofs("./result.ppm", std::ios::out | std::ios::binary);
     ofs << "P6\n" << width << " " << height << "\n255\n";
     for (unsigned i = 0; i < width * height; ++i) {
@@ -236,7 +295,7 @@ int main(int argc, char **argv)
 {
 	std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
     srand48(13);
-    std::vector<Sphere> spheres;
+    //std::vector<Sphere> spheres;
     // position, radius, farbe, reflektivität, transparenz, emission color
     spheres.push_back(Sphere(Vec3f( 0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0));
     spheres.push_back(Sphere(Vec3f( 0.0,      0, -20),     4, Vec3f(1.00, 0.32, 0.36), 1, 0.5));
@@ -245,7 +304,7 @@ int main(int argc, char **argv)
     spheres.push_back(Sphere(Vec3f(-5.5,      0, -15),     3, Vec3f(0.90, 0.90, 0.90), 1, 0.0));
     // light
     spheres.push_back(Sphere(Vec3f( 0.0,     20, -30),     3, Vec3f(0.00, 0.00, 0.00), 0, 0.0, Vec3f(3)));
-    render(spheres);
+    render();
 	std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
     
--- Raytracing/settings.h
+++ Raytracing/settings.h
@@ -2,3 +2,9 @@
 #define SHADOW_RAYS 30.0
 #define OFFSET_PERCENT 3.5
 #define MIN_SHADOW_BRIGHTNESS 0.15
+
+#define BLOCK_WIDTH 40
+#define BLOCK_HEIGHT 30
+#define width 640
+#define height 480
+#define THREAD_COUNT 4
