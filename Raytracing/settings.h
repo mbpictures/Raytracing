b5833a9a2456cee3eb0b02396b5ef75e16002cd1
vlc <clva1011@hs-karlsruhe.de>
2019-05-25 19:24:31 +0200

    changed test setup

--- Raytracing/settings.h
+++ Raytracing/settings.h
@@ -1,5 +1,5 @@
 #define MAX_RAY_DEPTH 12
-#define SHADOW_RAYS 30.0
+#define SHADOW_RAYS 1000.0
 #define OFFSET_PERCENT 3.5
 #define MIN_SHADOW_BRIGHTNESS 0.15
 
@@ -7,4 +7,4 @@
 #define BLOCK_HEIGHT 30
 #define width 640
 #define height 480
-#define THREAD_COUNT 4
+#define THREAD_COUNT 8
